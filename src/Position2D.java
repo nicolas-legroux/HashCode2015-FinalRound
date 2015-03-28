a1ac78f2074e0e5d8d3f0afaad73ed2f7d3615cc
G. Endignoux <ggendx@gmail.com>
2015-03-28 11:47:25 +0100

    Hash functions

--- src/Position2D.java
+++ src/Position2D.java
@@ -9,4 +9,20 @@ public class Position2D {
 		this.y = y;
 	}
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Position2D)) return false;
+        Position2D other = (Position2D) o;
+        return x == other.x && y == other.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 0;
+        result = 61 * result + x;
+        result = 61 * result + y;
+        return result;
+    }
+
 }
--- src/Position3D.java
+++ src/Position3D.java
@@ -9,4 +9,20 @@ public class Position3D {
 		this.z = z;
 	}
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Position3D)) return false;
+        Position3D other = (Position3D) o;
+        return pos.equals(other.pos) && z == other.z;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 0;
+        result = 61 * result + pos.hashCode();
+        result = 61 * result + z;
+        return result;
+    }
+
 }
