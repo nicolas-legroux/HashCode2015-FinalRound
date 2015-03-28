25cd389410b7c4617144ad456da48d3cdaa3d00d
Hugo Braun <hugo.v.braun@gmail.com>
2015-03-28 14:23:47 +0100

    DivisionSolver

--- src/Test.java
+++ src/Test.java
@@ -10,12 +10,14 @@ public class Test {
 			
 			pb.print();
 
-			BadSolver solver = new BadSolver(pb);
+			//BadSolver solver = new BadSolver(pb);
 			//Solver solver = new Solver(pb);
 			
-			Solution solution = solver.solve();
+			DivisionSolver ds = new DivisionSolver(pb);
+			
+			//Solution solution = solver.solve();
 			//solution.print();
-			solution.save("data/output.txt");
+			//solution.save("data/output.txt");
 			
 		} catch (IOException e) {
 			System.err.println("Error while doing IO stuff : " + e.getMessage());
